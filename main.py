9b8c97c5f4257cfd745048109c21e1290ebe3b8b
Marina Sosorova <msosorova05@gmail.com>
2024-12-24 22:22:09 +0700

    Добавлена функция maclaurin_ln_1_minus_x для вычисления ln(1 - x)

--- main.py
+++ main.py
@@ -9,3 +9,15 @@ def maclaurin_sin(x, n=10):
     for k in range(n):
         result += ((-1)**k * x**(2*k + 1)) / math.factorial(2*k + 1)
     return result
+
+
+def maclaurin_ln_1_minus_x(x, n=10):
+    """
+    Вычисление ln(1 - x) через ряд Маклорена
+    """
+    if abs(x) >= 1:
+        raise ValueError("Значение x должно быть в диапазоне |x| < 1.")
+    result = 0
+    for k in range(1, n + 1):
+        result -= x**k / k
+    return result
